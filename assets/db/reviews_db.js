3e927eecdf7c47aa6222d9c024f4074988f8603f
ErikQBirch <erikqbirch@gmail.com>
2023-02-21 10:30:30 -0700

    reorganized Reviews again

--- assets/db/reviews_db.js
+++ assets/db/reviews_db.js
@@ -16,19 +16,19 @@ export const reviews = [
     "approved":true
   },
   {
-    "reviewer":"Ben Griswald",
-    "rating":5,
-    "starCount": "⭐⭐⭐⭐⭐",
-    "reviewDate":"Wed Dec 7 2022",
-    "reviewText":"Kyle did a wonderful job cleaning my truck. His price was extremely reasonable for the great service he provided. I was nothing but happy and impressed by the change my truck went through. I didn't even think it was that dirty, but he proved me wrong. He showed me a ton of the things you can't really see on the outside. He was always quick to respond and extremely easy to work with. One of the nicest guys you'll meet. I would highly recommend his services to anyone.",
-    "approved":true
-  },
-  {
     "reviewer":"Noah Agulara",
     "rating":5,
     "starCount": "⭐⭐⭐⭐⭐",
     "reviewDate":"Thurs Dec 8 2022",
     "reviewText":"Kyle takes great care of his customers! He leaves little note card for those he serves and he does great work. Would 100% recommend!!!",
+    "approved":true
+  },
+  {
+    "reviewer":"Ben Griswald",
+    "rating":5,
+    "starCount": "⭐⭐⭐⭐⭐",
+    "reviewDate":"Wed Dec 7 2022",
+    "reviewText":"Kyle did a wonderful job cleaning my truck. His price was extremely reasonable for the great service he provided. I was nothing but happy and impressed by the change my truck went through. I didn't even think it was that dirty, but he proved me wrong. He showed me a ton of the things you can't really see on the outside. He was always quick to respond and extremely easy to work with. One of the nicest guys you'll meet. I would highly recommend his services to anyone.",
     "approved":true
   }
 ]
